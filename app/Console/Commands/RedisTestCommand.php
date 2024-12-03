d2e0e56f9a07bd7c3db1eb1a801ddf8de27ab9e4
Yaroslav-Pahomov <pahomstyle90@mail.ru>
2024-12-03 11:36:59 +0500

    Redis cache настройка и подготовка
    1.Выбор инструмента кэша (Redis) в файле .env "CACHE_STORE=redis" (выбрать можно в файле config/cache.php из массива stores);
    
    2. Создание консольной команды для тестирования (make:command RedisTestCommand);
    
    3. Настройка префикса и наименования (в файле .env "REDIS_PREFIX=redis:", указан в config/database.php).

---
+++ app/Console/Commands/RedisTestCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Cache;
+
+class RedisTestCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'redis:go';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        Cache::put('key_test', 'value_test');
+        Cache::put('key_test_1', 'value_test_1');
+        dd('Redis command');
+    }
+}
